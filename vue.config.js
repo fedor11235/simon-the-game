ddd9d20828856994337c9546e095038f54904e9b
fedor11235 <f.avdeev@list.ru>
2022-01-28 00:13:12 +0300

    init

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+module.exports = {
+	configureWebpack: {
+		resolve: {
+			alias: {
+				"@components": path.resolve(__dirname, 'src', 'components'),
+				"@styles": path.resolve(__dirname, 'src', 'styles'),
+				"@sound": path.resolve(__dirname, 'src', 'assets', 'sounds')
+			}
+		}
+	}
+}
